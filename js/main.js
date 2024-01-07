ee65ab5a3df8ad7160e3dc6d89ed840b438ac5ce
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-07 22:26:29 +0000

    Update footer anime img import mobile menu js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+import mobileMenu from "./mobileMenu";
 import projectsPopulation from "./projectsPopulation";
 import skillsPopulation from "./skillsPopulation";
 
 projectsPopulation();
 skillsPopulation();
+mobileMenu();
