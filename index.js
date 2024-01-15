afba55c9c0ed13abcc5bec8dd729dc4c1d17ddbd
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-15 08:39:52 +0000

    Add a loading animation

--- index.js
+++ index.js
@@ -514,6 +514,15 @@ const mobileMenu = () => {
     });
 }
 
+window.addEventListener("load", ()=> {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader--hidden");
+    loader.addEventListener("transitionend", ()=> {
+        document.body.removeChild(loader);
+    })
+});
+
 projectsPopulation();
 skillsPopulation();
 mobileMenu();
